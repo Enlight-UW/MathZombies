22d1982f50c9813824746abae29d13c026fec9e7
unknown <klklassy@wisc.edu>
2016-03-17 23:15:12 -0500

    Zombie spawner added; rotate zombies to face center point while walking

--- Assets/Scripts/MoveTowards.cs
+++ Assets/Scripts/MoveTowards.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class MoveTowards : MonoBehaviour {
 
     public Transform target;
-    public float speed = 5;
-
+    public float speed = 2;
+   
     // Update is called once per frame
     void Update() {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        Vector3 target = new Vector3(0, 0, 0);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        /*
+        if (transform.position == target)
+        {
+            // call health down function
+            Destroy(gameObject);
+            return;
+        }
+        */
+
     }
 
 }
--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -3,22 +3,42 @@ using System.Collections;
 
 public class ZombieSpawner : MonoBehaviour {
 
+
     // z goes from 0 to -18 or so
     // x goes from 0 
 
     public int maxZombies = 20;
     public GameObject zombie;
-    public float radius = 9;
+    public float spawnTime = 3f;
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
 
-    
+    public Vector3[] spawnPoints = new Vector3[5];
+  
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //private Vector3 originPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        spawnPoints[0] = new Vector3(0, 0, -10);
+        spawnPoints[1] = new Vector3(23, 4, 5);
+        spawnPoints[2] = new Vector3(-18, 0, -13);
+        spawnPoints[3] = new Vector3(0, 0, 16);
+        spawnPoints[4] = new Vector3(10, 2, -5);
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+
+        int spawnPointIndex = Random.Range(0, 4);
+        Vector3 randomPosition = spawnPoints[spawnPointIndex];
+
+        Instantiate(zombie, randomPosition, Quaternion.Euler(0, 0, 0));
+
+    }
 }
