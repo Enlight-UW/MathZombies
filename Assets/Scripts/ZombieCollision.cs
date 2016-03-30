cd4d596e6645919f0963c562074f9598e8acf3d0
benwebsta <bmwebster@wisc.edu>
2016-03-29 19:18:50 -0500

    Got zombie collision

---
+++ Assets/Scripts/ZombieCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Enemy")
+			Destroy(other.gameObject);    
+	}
+}
